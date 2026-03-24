97fac5837f337862dbcdeb7ddf4013a171efd958
SHARVAN223 <sharvan70458@gmail.com>
2026-03-24 13:03:35 +0530

    68202

--- home/serializers.py
+++ home/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from .models import MenuCategory
+
+class Menu_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model= MenuCategory
+        fields = ['name']
+        
