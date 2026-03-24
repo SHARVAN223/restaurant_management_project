fdf95ae8e4aacc666e456b8817e47ccdb087e0d9
SHARVAN223 <sharvan70458@gmail.com>
2026-03-24 13:09:25 +0530

    68202

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .model import Menucategory
-from .serializers import Menu_se
+from .serializers import Menu_Serializer
+
 # Create your views here.
 
 
 def Menucategory(req):
+    data = Menucategory
